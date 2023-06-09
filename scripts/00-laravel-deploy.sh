27093a6a686263af207014c44fbd9e5633b8d305
Isaac Ubani <32121772+ubaniIsaac@users.noreply.github.com>
2023-06-09 23:06:06 +0100

    fix: run passport:install in start for oauth key

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+composer update
+composer install --no-dev
 
 echo "Caching config..."
 php artisan config:cache
+
 
 echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Passport install..."
+php artisan passport:install
